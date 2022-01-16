0c23e1268d56a65ea80b253384a791a3e368b719
sbtah <shisastore@protonmail.com>
2022-01-16 16:13:29 +0100

    Modified both ProjectDetailSerializer and InvestorDetailSerializer to display matching Investor, Project.

--- recruitment_task/core/serializers.py
+++ recruitment_task/core/serializers.py
@@ -17,15 +17,23 @@ class InvestorSerializer(serializers.ModelSerializer):
         read_only_fields = ["remaining_amount"]
 
 
+# Added list_inventors for listing Investors that can match with Project.
 class ProjectDetailsSerializer(serializers.ModelSerializer):
+
+    list_investors = serializers.ListField(required=False)
+
     class Meta:
         model = Project
         fields = "__all__"
-        read_only_fields = ["funded", "funded_by"]
+        read_only_fields = ["funded", "funded_by", "list_investors"]
+        
 
-
+# Added list_projects for listing Projects that can match with Investor. 
 class InvestorDetailsSerializer(serializers.ModelSerializer):
+
+    list_projects = serializers.ListField(required=False)
+   
     class Meta:
         model = Investor
         fields = "__all__"
-        read_only_fields = ["remaining_amount"]
+        read_only_fields = ["remaining_amount", "list_projects "]
