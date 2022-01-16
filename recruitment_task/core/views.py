99d461c4e69ad9e5a84a4100c2e9242ae9993cab
sbtah <shisastore@protonmail.com>
2022-01-16 15:55:49 +0100

    Added MatchingProjects and MatchingIvestors views.

--- recruitment_task/core/views.py
+++ recruitment_task/core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 
 from core.businesslogic.errors import CannotInvestIntoProjectException
 from core.businesslogic.investing import invest_into_project
@@ -82,3 +84,33 @@ class InvestIntoProject(APIView):
                 "remaining_amount": investor.remaining_amount
             }
         )
+
+
+# Investor's endpoint for matching with Projects.
+class MatchingProjectsView(generics.ListAPIView):
+    """List of matching Projects for Investor"""
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectDetailsSerializer
+
+    def get(self, request, pk):
+        """Custom get method with complex lookup with Q object."""
+        investor = get_object_or_404(Investor,pk=pk)
+        items = Project.objects.filter(Q(funded=False) & Q(amount__lte=investor.remaining_amount) & Q(amount__lte=investor.individual_amount) & Q(delivery_date__lte=investor.project_delivery_deadline))
+        serializer = ProjectDetailsSerializer(items, many=True) # I may lack knowledge here, why there are 2 types of serializers for model?
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+# Projects's endpoint for matching with Investors.
+class MathingInvestorsView(generics.ListAPIView):
+    """List of matching Investors for Project."""
+
+    queryset = Investor.objects.all()
+    serializer_class = InvestorDetailsSerializer
+
+    def get(self, request, pk):
+        """Custom get method with complex lookup with Q object."""
+        project = get_object_or_404(Project, pk=pk)
+        items = Investor.objects.filter(Q(remaining_amount__gte=project.amount) & Q(individual_amount__gte=project.amount) & Q(project_delivery_deadline__gte=project.delivery_date))
+        serializer = InvestorDetailsSerializer(items, many=True) # I may lack knowledge here, why there are 2 types of serializers for model?
+        return Response(serializer.data, status.HTTP_200_OK)
